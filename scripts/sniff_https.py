d19161e0b19a9c21093cd33f6aa72656f602932e
yurism <mascarenhasyuri@hotmail.com>
2024-07-07 20:14:01 -0300

    [Fix] File writing

--- scripts/sniff_https.py
+++ scripts/sniff_https.py
@@ -7,13 +7,14 @@ def packet_callback_https(packet):
         if packet.haslayer(TCP) and packet[TCP].dport == 443:
             # Log the HTTPS packet details into a text file
             with open(f"../sniffer-logs/https/https_sniffer_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt", 'a') as file:
-                file.write(f"[*] HTTPS Packet - {datetime.now()}\n")ile
-                file.write(f"Source IP: {packet[IP].src}\n")ile
-                file.write(f"Destination IP: {packet[IP].dst}\n")ile
-                file.write(f"Source Port: {packet[TCP].sport}\n")ile
-                file.write(f"Destination Port: {packet[TCP].dport}\n")ile
-                file.write(f"Sequence Number: {packet[TCP].seq}\n")ile
-                file.write(f"Acknowledgment Number: {packet[TCP].ack}\n")ile
+                file.write(f"[*] HTTPS Packet - {datetime.now()}\n")
+                if packet.haslayer(IP):
+                    file.write(f"Source IP: {packet[IP].src}\n")
+                    file.write(f"Destination IP: {packet[IP].dst}\n")
+                file.write(f"Source Port: {packet[TCP].sport}\n")
+                file.write(f"Destination Port: {packet[TCP].dport}\n")
+                file.write(f"Sequence Number: {packet[TCP].seq}\n")
+                file.write(f"Acknowledgment Number: {packet[TCP].ack}\n")
                 file.write(f"Flags: {packet[TCP].flags}\n")
                 # Log the payload if it exists
                 if Raw in packet:
